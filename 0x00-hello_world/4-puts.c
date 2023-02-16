df96ff913ecbd6f663a0a4d5b3a2674dbc9f0fba
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-16 11:32:35 -0800

    4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: o
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle"\n);
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
