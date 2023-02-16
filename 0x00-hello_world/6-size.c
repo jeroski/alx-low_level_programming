c998cfcda8996ba01a3c519a8d17a2d521c6c290
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-16 13:48:41 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: o
+ */
+int main(void)
+{
+	printf("size of a char is=%i bity,\n"sizeof (char);
+	 printf("size of an int is=%i bitys,\n"sizeof (int);
+	  printf("size of a long int is=%i bitys,\n"sizeof (long int);
+	   printf("size of a long long int is=%i bitys,\n"sizeof (long long int);
+	    printf("size of a float is=%i bitys,\n"sizeof (float);
+	return (0);
+}
