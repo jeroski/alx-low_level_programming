3f33a09e9f183fd1abf9a21983f0242d1fd21c26
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-16 12:38:48 -0800

    my printf functiun

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - main block
+ * Return: o
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
- 
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
