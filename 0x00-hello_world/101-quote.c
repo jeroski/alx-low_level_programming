9e6c8b295e24b21c3eb25b65cac7fd6bda69d00f
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-17 01:59:40 -0800

    but you have to be a genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - main block
  * Return: 1
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful - Dora Korpar, 2015-10-19,\n");
+	fwrite(2 "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
