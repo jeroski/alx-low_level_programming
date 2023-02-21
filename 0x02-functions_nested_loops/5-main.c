94987697a7c7df7c84e47d516b0a157c018702ac
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-21 13:48:43 -0800

    task 5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -9,22 +9,22 @@ int main(void)
 {
     int r;
 
-    r = print_sign(98);
+    r = print_sing(98);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(0);
+    r = print_sing(0);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(0xff);
+    r = print_sing(0xff);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(-1);
+    r = print_sing(-1);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
