a4b10f6a87ddec337ef52abae4d32952bad45cef
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-21 05:53:35 -0800

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,16 @@ void print_alphabet_x10(void)
 	int i = 0;
 
 	while (i <= 9)
+	{
 
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
+		}
 
 	_putchar('\n');
 
 	i++;
+
+	}
 }
