06712ac42773bda77cb12cf1bdd9f2b30c52a460
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-02-27 07:54:53 -0800

    my task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * rev_string - reverse a string
- * @s: print reverse
- *
- * Return: 0
- */
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
 void rev_string(char *s)
 {
 	int i, c, k;
