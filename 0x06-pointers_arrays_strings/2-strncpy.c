3c11eb286526d7d85de885e214750ffb56c9f1bc
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-03-01 12:48:50 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy -> for copying purpose
+ * @dest: parameter1
+ * @src: parameter2
+ * @n: parameter3
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
