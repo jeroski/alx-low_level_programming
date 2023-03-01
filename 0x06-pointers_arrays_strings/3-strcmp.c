db600d172a99d1ca412b8150ae4a83a5b2400e23
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-03-01 13:07:14 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - to compare a string
+ *@s1: parameter1
+ *@s2: parameter2
+ *Return: string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	
+	result = strcmp(s1, s2);
+	return (result);
+}
