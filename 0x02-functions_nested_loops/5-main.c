e303b8d04aadeb66beb3a4f8fa5b103213b6fa15
Apenutsui Jeroski Eli <elijeroski@example.com>
2023-03-31 08:31:13 -0700

    0. Print

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -9,22 +9,22 @@ int main(void)
 {
     int r;
 
-    r = print_sing(98);
+    r = print_sign(98);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sing(0);
+    r = print_sign(0);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sing(0xff);
+    r = print_sign(0xff);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sing(-1);
+    r = print_sign(-1);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_sign - Prints the sign of a number
+  * print_sign - Prints the signed of a number
   * @n: The number to be checked
   *
   * Return: 1 for positive num, -1 for negative num or zero for anything else
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (o);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		_putchar('_');
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 }
